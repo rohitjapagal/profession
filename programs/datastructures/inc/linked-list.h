1cf8146185d9b14eebe1fd628cf745807164d1eb
rangarajan <rrangarajan.85@gmail.com>
2016-03-02 07:11:06 -0800

    linked list loops

--- programs/datastructures/inc/linked-list.h
+++ programs/datastructures/inc/linked-list.h
@@ -1,6 +1,13 @@
+
 #ifndef __LinkedList_H__
 
 #define __LinkedList_H__
+#include <iostream>
+#ifdef DEBUG
+    #define DEBUG_PRINT(x) printf x
+#else
+    #define DEBUG_PRINT(x) do {} while (0)
+#endif
 
 int add (int,int);
 
